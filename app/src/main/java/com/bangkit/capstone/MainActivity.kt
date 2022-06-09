c717df2c71a81d13e118a5ee7b86a0ec6b30cf17
Zaenal Arif <zaenalarif05091999@gmail.com>
2022-06-10 02:22:27 +0700

    add and remove: 1. action_bar_title_layout.xml 2. font_certs.xml 3. preloaded_fonts.xml (inter-Bold) 4. purple: Primary & PrimaryVariant to blue -> themes.xml & themes.xml(night) 4. blue color #0038FF (colors.xml)

--- app/src/main/java/com/bangkit/capstone/MainActivity.kt
+++ app/src/main/java/com/bangkit/capstone/MainActivity.kt
@@ -2,10 +2,15 @@ package com.bangkit.capstone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setCustomView(R.layout.action_bar_title_layout)
+
     }
 }
