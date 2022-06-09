70112ffcefe0f0e6e07a13484e63d0df35547121
Zaenal Arif <zaenalarif05091999@gmail.com>
2022-06-10 02:31:14 +0700

    add icon search in action bar title (Fake News)

--- app/src/main/java/com/bangkit/capstone/MainActivity.kt
+++ app/src/main/java/com/bangkit/capstone/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bangkit.capstone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
         supportActionBar?.setCustomView(R.layout.action_bar_title_layout)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.search, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
