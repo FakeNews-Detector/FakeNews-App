74b1faf868cb988adc43cb30b346aca58dd51544
Zaenal Arif <zaenalarif05091999@gmail.com>
2022-06-11 11:58:35 +0700

    add activity_splash_screen.xml with dependency in gradle(module)

---
+++ app/src/main/java/com/bangkit/capstone/ui/SplashScreenActivity.kt
@@ -0,0 +1,27 @@
+package com.bangkit.capstone.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.bangkit.capstone.MainActivity
+import com.bangkit.capstone.R
+import com.bangkit.capstone.databinding.ActivitySplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var splashScreenBinding: ActivitySplashScreenBinding
+    private var timeEstimate: Long = 2860
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        splashScreenBinding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(splashScreenBinding.root)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, timeEstimate)
+    }
+}
